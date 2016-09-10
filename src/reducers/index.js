51ef46a116b8bfa12063d16eb278476c430b675b
Damien McMahon <damien@macoto.co.uk>
2016-09-10 14:51:33 +0100

    Redux bollox added for extra cool points and wow!

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import netWorth from './net-worth';
+
+const netWorthApp = combineReducers({
+  netWorth
+});
+
+export default netWorthApp;
---
+++ src/reducers/net-worth.js
@@ -0,0 +1,8 @@
+const netWorth = (state = {}, action) => {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
+
+export default netWorth;
